b3bbbd5553742f35d5cc252935552151f01b108f
Tapan Thaker <tapan.d.thaker@gmail.com>
2016-03-10 20:47:41 +0530

    Initial Commit

---
+++ Pow/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Pow
+//
+//  Created by Tapan Thaker on 10/03/16.
+//  Copyright © 2016 TT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
